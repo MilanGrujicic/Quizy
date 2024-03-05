f43962413d273d6ed01f214772a6abd3fb57d5a0
Milan <milangru@gmail.com>
2024-03-05 19:44:51 +0100

    Added open_options function

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from tkinter import *
 
+# BUTTON FUNCTIONS
+def open_options():
+    options = Toplevel(root)
+    options.title("Quizy - Options")
+    options.config(bg="#27262e")
+    options.geometry("360x225")
+
 # MAIN WINDOWN SETUP
 root = Tk()
 root.title("Quizy")
@@ -15,7 +22,7 @@ canvas.create_image(200, 120, image=logo)
 # BUTTONS
 b_start = Button(text='Start Game', width=10)
 b_start.grid(row=1, column=0, padx= 125, pady=5)
-b_options = Button(text='Option', width=10)
+b_options = Button(text='Option', width=10, command=open_options)
 b_options.grid(row=2, column=0, pady=5)
 b_exit = Button(text='Exit', width=10, command=root.destroy)
 b_exit.grid(row=3, column=0, pady=5)
