09b61757555cb1ddf11a43446acff3e19e2a8946
Milan <milangru@gmail.com>
2024-03-20 09:43:40 +0100

    Added label to start game page

--- main.py
+++ main.py
@@ -17,14 +17,17 @@ def start_game():
     slovenia_flag = PhotoImage(file="Flags/slo_flag.png")
     china_flag = PhotoImage(file="Flags/chinese_flag.png")
     
+    l_pick_country = Label(start_game_window, text="Pick A Country", font=("Arial", 15, "bold"), fg='black', bg="#39dbff")
+    l_pick_country.grid(row=0, column=0)
+
     b_level_1 = Button(start_game_window, text="Brazil", image=brazil_flag, compound="top", padx=30, bg="#fff500", highlightbackground = "black", bd=100, border="2",command=lambda: start_chosen_map("brazil"))
-    b_level_1.grid(row=0, column=0, padx=125, pady=5)
+    b_level_1.grid(row=1, column=0, padx=125, pady=5)
     b_level_2 = Button(start_game_window, text="Slovenia", image=slovenia_flag, compound="top", padx=27, bg="#fff500", highlightbackground = "black", bd=100, border="2", command=lambda: start_chosen_map("slovenia"))
-    b_level_2.grid(row=1, column=0, padx=125, pady=5)
+    b_level_2.grid(row=2, column=0, padx=125, pady=5)
     b_level_3 = Button(start_game_window, text="China", image=china_flag, compound="top", padx=30, bg="#fff500", highlightbackground = "black", bd=100, border="2", command=lambda: start_chosen_map("china"))
-    b_level_3.grid(row=2, column=0, padx=125, pady=5)
+    b_level_3.grid(row=3, column=0, padx=125, pady=5)
     b_exit = Button(start_game_window, text="Back", width=10, compound="left", padx=14, bg="#fff500", highlightbackground = "black", bd=100, border="2", command=start_game_window.destroy)
-    b_exit.grid(row=3, column=0, padx=125, pady=25)
+    b_exit.grid(row=4, column=0, padx=125, pady=25)
     
     start_game_window.mainloop()
 
