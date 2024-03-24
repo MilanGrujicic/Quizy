9218d642c93b6c60cb3da7e03866c5f68e0b3a27
Milan <milangru@gmail.com>
2024-03-24 20:49:58 +0100

    Added docstrings to utils.py

--- utils.py
+++ utils.py
@@ -3,12 +3,14 @@ from tkinter import messagebox
 import subprocess
 
 def open_options():
+    '''From initial screen, opens Options dialog.'''
     options_dialog = Toplevel()
     options_dialog.title("Quizy - Options")
     options_dialog.config(bg="#39dbff")
     options_dialog.geometry("360x225")
 
 def start_game():
+    '''From initial screen, opens start game screen.'''
     start_game_window = Toplevel()
     start_game_window.title("Pick a country")
     start_game_window.config(bg="#39dbff")
@@ -33,9 +35,11 @@ def start_game():
     start_game_window.mainloop()
 
 def start_chosen_map(country, division):
+    '''From start game screen, opens the country map picked by the user.'''
     open_instructions(country, division)
     file_to_run = f"{country}.py"
     subprocess.run(["python3", file_to_run])
 
 def open_instructions(contry, division):
+    '''Displays dialog with instructions.'''
     messagebox.showinfo(title="Instructions", message=f"You will be prompted with the map of {contry.title()}.\nTry to guess all its {division}.\nHave fun.")
