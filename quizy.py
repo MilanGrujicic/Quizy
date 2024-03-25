6ba9048b32d99e9202fc7b2e32659b32654e50b6
Milan <milangru@gmail.com>
2024-03-25 22:41:29 +0100

    Renamed variables

--- quizy.py
+++ quizy.py
@@ -15,12 +15,12 @@ class Quizy:
         canvas.create_image(200, 120, image=logo)
 
         # BUTTONS
-        b_start = Button(text='Start Game', width=10, bg="#fff500", highlightbackground = "black", border="2", command=utils.start_game)
-        b_start.grid(row=1, column=0, padx= 125, pady=5)
-        b_options = Button(text='Options', width=10, bg="#fff500", highlightbackground = "black", border="2", command=utils.open_options)
-        b_options.grid(row=2, column=0, pady=5)
-        b_exit = Button(text='Exit', width=10, bg="#fff500", highlightbackground = "black", border="2", command=self.window.destroy)
-        b_exit.grid(row=3, column=0, pady=5)
+        start_game = Button(text='Start Game', width=10, bg="#fff500", highlightbackground = "black", border="2", command=utils.start_game)
+        start_game.grid(row=1, column=0, padx= 125, pady=5)
+        options = Button(text='Options', width=10, bg="#fff500", highlightbackground = "black", border="2", command=utils.open_options)
+        options.grid(row=2, column=0, pady=5)
+        exit = Button(text='Exit', width=10, bg="#fff500", highlightbackground = "black", border="2", command=self.window.destroy)
+        exit.grid(row=3, column=0, pady=5)
 
         # LABEL
         l_creator = Label(text='Made with love by Milan.', font=("Arial", 10, "italic"), fg='black', bg="#39dbff")
