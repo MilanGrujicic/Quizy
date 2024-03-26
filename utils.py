14ed520c91ead2a7c98e06f1e3955c1c48d4842b
Milan <milangru@gmail.com>
2024-03-26 16:08:32 +0100

    Added functions that handle both chinese maps

--- utils.py
+++ utils.py
@@ -14,10 +14,32 @@ def start_game():
     '''From initial screen, opens start game screen.'''
     start_game_screen = quizy.start_game()
 
+def open_chinese_map():
+    chinese_map = Toplevel()
+    chinese_map.title("Pick an option")
+    chinese_map.config(bg="#39dbff")
+    chinese_map.geometry("450x150")
+
+    pick_an_option = Label(chinese_map, text="Pick which map you want to play", font=("Arial", 15, "bold"), fg='black', bg="#39dbff")
+    pick_an_option.grid(row=0, column=0, padx=25, pady=25)
+
+    button_frame = Frame(chinese_map, bg="#39dbff")
+    button_frame.grid(row=1,column=0)
+
+    chinese_map_by_china = Button(button_frame, text="Chinese Map By China", bg="#fff500", highlightbackground = "black", bd=100, border="2", command=open_chinese_map_by_china)
+    chinese_map_by_china.pack(side=LEFT, padx=5)
+
+    chinese_map_by_western_countries = Button(button_frame, text="Chinese Map By Western Countries", bg="#fff500", highlightbackground = "black", bd=100, border="2", command=open_chinese_map_by_western_countries)
+    chinese_map_by_western_countries.pack(side=RIGHT, padx=5)
+
+def open_chinese_map_by_china():
+    start_chosen_map("china", "provinces")
+
+def open_chinese_map_by_western_countries():
+    start_chosen_map("china_by_western_countries", "provinces")
+
 def start_chosen_map(country, division):
     '''From start game screen, opens the country map picked by the user.'''
-    if country == "china":
-        chinese_map = quizy.chinese_map()
     open_instructions(country, division)
     file_to_run = f"{country}.py"
     subprocess.run(["python3", file_to_run])
