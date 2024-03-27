ff755cbc553185cbf13dbd62332d27ce1d8010b2
Milan <milangru@gmail.com>
2024-03-27 19:34:07 +0100

    Removed options button

--- quizy.py
+++ quizy.py
@@ -17,14 +17,12 @@ class Quizy:
         # BUTTONS
         start_game = Button(text='Start Game', width=10, bg="#fff500", highlightbackground = "black", border="2", command=utils.start_game)
         start_game.grid(row=1, column=0, padx= 125, pady=5)
-        options = Button(text='Options', width=10, bg="#fff500", highlightbackground = "black", border="2", command=utils.open_options)
-        options.grid(row=2, column=0, pady=5)
         exit = Button(text='Exit', width=10, bg="#fff500", highlightbackground = "black", border="2", command=self.window.destroy)
-        exit.grid(row=3, column=0, pady=5)
+        exit.grid(row=2, column=0, pady=5)
 
         # LABEL
         l_creator = Label(text='Made with love by Milan.', font=("Arial", 10, "italic"), fg='black', bg="#39dbff")
-        l_creator.grid(row=4, column=0, sticky="SW", pady=40)
+        l_creator.grid(row=3, column=0, sticky="SW", pady=80)
 
         self.window.mainloop()
 
